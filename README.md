7fb4453ad1954be96d85647e84e385a05192bc8f
Craig Spaeth <craigspaeth@gmail.com>
2014-02-17 11:04:58 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# emoji-clipboard
+
+Pulls an emojicon from [emojicons.com](http://emojicons.com) and copies it to your clipboard.
+
+1. `npm install emoji-clipboard -g`
+2. `emojic flipping-tables`
+3. Cmd + V and paste your table flip!
---
+++ bin/emojic.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var request = require('superagent'),
+    exec = require('child_process').exec,
+    cheerio = require('cheerio');
+
+var emoji = process.argv[2];
+
+request.get('http://emojicons.com/e/' + emoji).end(function(res) {
+  $ = cheerio.load(res.text);
+  if (res.err) return console.log(res.err);
+  exec("echo " + $('.emoticon-item textarea').text() + " | pbcopy", function() {
+    console.log("Copied!");
+  })
+});
