95fd1b06024a61412c2878b934b985dc0d3b7e23
Craig Spaeth <craigspaeth@gmail.com>
2014-02-17 11:09:09 -0500

    need them quotes

--- bin/emojic.js
+++ bin/emojic.js
@@ -9,7 +9,7 @@ var emoji = process.argv[2];
 request.get('http://emojicons.com/e/' + emoji).end(function(res) {
   $ = cheerio.load(res.text);
   if (res.err) return console.log(res.err);
-  exec("echo " + $('.emoticon-item textarea').text() + " | pbcopy", function() {
+  exec("echo \"" + $('.emoticon-item textarea').text() + "\" | pbcopy", function() {
     console.log("Copied!");
   })
 });
