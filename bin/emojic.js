3681329218170b210a9bc5bf8e29692ae9b37f47
Craig Spaeth <craigspaeth@gmail.com>
2014-02-18 13:38:13 -0500

    search by tags

--- bin/emojic.js
+++ bin/emojic.js
@@ -7,9 +7,21 @@ var request = require('superagent'),
 var emoji = process.argv[2];
 
 request.get('http://emojicons.com/e/' + emoji).end(function(res) {
-  $ = cheerio.load(res.text);
-  if (res.err) return console.log(res.err);
-  exec("echo \"" + $('.emoticon-item textarea').text() + "\" | pbcopy", function() {
-    console.log("Copied!");
-  })
+  if (res.err) return console.warn(res.err);
+  if (res.status == 404) {
+    request.get('http://emojicons.com/tag/' + emoji).end(function(res) {
+      var $ = cheerio.load(res.text);
+      var text = $('.emoticon-item:first-child textarea').text().trim();
+      text ? cp(text) : console.log("No emojis found (◞‸◟；)");
+    });
+  } else {
+    var $ = cheerio.load(res.text);
+    cp($('.emoticon-item textarea').text());
+  }
 });
+
+var cp = function (text) {
+  exec("echo \"" + text + "\" | pbcopy", function() {
+    console.log("Copied!");
+  });
+}
